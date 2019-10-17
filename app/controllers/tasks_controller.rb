8a6df743efdbd0d029853ff5d3605ebcf5991fe3
ikkou.nobuyuki <ikko1550@gmail.com>
2019-10-17 11:57:58 +0000

    kadai-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,20 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show,:update,:destroy,:edit]
   
-  before_action :set_task,only:[:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def show
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Taskが正常に作成されました"
@@ -45,13 +46,18 @@ class TasksController < ApplicationController
     redirect_to tasks_url
   end
   
-  private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  
+  private
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
